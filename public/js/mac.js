9c41cfd623c3f7a82933fa5060f7b247808add72
tigerdone <3196083633@qq.com>
2018-10-15 01:36:33 +0800

    day1015

---
+++ public/js/mac.js
@@ -0,0 +1,7 @@
+if(screen.width > 1900&&window.devicePixelRatio >= 2){
+    setStyleSheet()
+}
+function setStyleSheet() {
+    document.getElementById("a").setAttribute("rel", "stylesheet");
+    document.getElementById("b").setAttribute("rel", "alternate");
+}
--- public/js/movelink.js
+++ public/js/movelink.js
@@ -1,6 +1,25 @@
+// $(document).load(function () {
+//     var height = 530;
+//     if(screen.width > 1900&&window.devicePixelRatio >= 2){
+//         height = 265;
+//     }
+//     window.onscroll = function() {
+//         var scrollY = window.scrollY;
+//
+//         if(scrollY > height){
+//             $(".other_linker").addClass("other_linker2");
+//         }else {
+//             $(".other_linker").removeClass("other_linker2");
+//         }
+//     }
+// })
 window.onscroll = function() {
     var scrollY = window.scrollY;
-    if(scrollY > 530){
+    var height = 530;
+    if(screen.width > 1900&&window.devicePixelRatio >= 2){
+        height = 270;
+    }
+    if(scrollY > height){
         $(".other_linker").addClass("other_linker2");
     }else {
         $(".other_linker").removeClass("other_linker2");
