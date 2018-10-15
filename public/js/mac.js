f91007100eb8ed48c576fe5b09d6572897a2114c
tigerdone <3196083633@qq.com>
2018-10-15 18:21:36 +0800

    day 1015

--- public/js/mac.js
+++ public/js/mac.js
@@ -5,3 +5,8 @@ function setStyleSheet() {
     document.getElementById("a").setAttribute("rel", "stylesheet");
     document.getElementById("b").setAttribute("rel", "alternate");
 }
+if(screen.width >= 1280 && screen.width < 1440&&window.devicePixelRatio >= 2){
+    document.getElementById("a1").setAttribute("rel","stylesheet");
+    document.getElementById("b").setAttribute("rel","alternate");
+
+}
