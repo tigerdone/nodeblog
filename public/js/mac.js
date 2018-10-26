6cd6c80fdd42a736d550c4839c2c5303939958b2
tigerdone <3196083633@qq.com>
2018-10-26 13:13:21 +0800

    666

--- public/js/mac.js
+++ public/js/mac.js
@@ -1,12 +1,25 @@
-if(screen.width > 1900&&window.devicePixelRatio >= 2){
+if(goPAGE() == "pc"&&window.devicePixelRatio >= 1.5){
     setStyleSheet()
 }
 function setStyleSheet() {
     document.getElementById("a").setAttribute("rel", "stylesheet");
     document.getElementById("b").setAttribute("rel", "alternate");
 }
-if(screen.width >= 1280 && screen.width < 1440&&window.devicePixelRatio >= 2){
-    document.getElementById("a1").setAttribute("rel","stylesheet");
-    document.getElementById("b").setAttribute("rel","alternate");
 
+// if(screen.width >= 1280 && screen.width < 1440&&window.devicePixelRatio >= 2){
+//     document.getElementById("a1").setAttribute("rel","stylesheet");
+//     document.getElementById("b").setAttribute("rel","alternate");
+//
+// }
+
+
+function goPAGE(){
+    if((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+        // window.location.href="移动端url";
+        return "mobile"
+    }
+    else {
+        // window.location.href="pc端url";
+        return "pc"
+    }
 }
